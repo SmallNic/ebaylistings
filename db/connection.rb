90691a75e54c20f8d53cd3f9b70b563a6f4d1d58
SmallNic <nas231@nyu.edu>
2015-10-04 00:19:45 -0400

    altered connection file to configure production environment

--- db/connection.rb
+++ db/connection.rb
@@ -4,10 +4,28 @@ require 'active_record'
 # local db name
 # :database => "ebaylistings"
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "postgresql",
-  :database => "ebaylistings"
-)
+configure :development do
+
+ ActiveRecord::Base.establish_connection(
+   :adapter => "postgresql",
+   :database => "ebaylistings"
+ )
+end
+
+configure :production do
+ db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+ ActiveRecord::Base.establish_connection(
+   :adapter => "postgresql",
+   :host => db.host,
+   :username => db.user,
+   :password => db.password,
+   :database => db.path[1..-1],
+   :encoding => 'utf8'
+ )
+end
+
+
 
 if defined?(Sinatra)
   # Fix an issue with sinatra and Active Record where connections are left open
